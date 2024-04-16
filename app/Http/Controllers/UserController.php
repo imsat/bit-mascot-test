12a6f06e3380ccba8e5117ac9dd86c1f4d0383c6
imsat <satyajit.cse@yahoo.com>
2024-04-17 03:13:28 +0600

    change password done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -25,6 +26,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
+        abort_unless(auth()->id() === $user->id, '419', 'Unauthorized');
         $user->load('userInfo:user_id,address,phone,dob,nid');
         return view('pages.user.show', compact('user'));
     }
@@ -35,5 +37,30 @@ class UserController extends Controller
     public function showChangePassword()
     {
         return view('pages.user.change-password');
+    }
+
+    /**
+     * Show change user password form.
+     */
+    public function changePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required|string',
+            'new_password' => 'required|string',
+            'confirm_password' => 'required|string',
+        ]);
+
+        $user = $request->user();
+        $data = $request->only('old_password', 'new_password', 'confirm_password');
+
+        if (Hash::check($data['old_password'], $user->password)) {
+            $user->update([
+                'password' => Hash::make($data['confirm_password']),
+            ]);
+            return response()->json(['message' => 'Password changed successfully.', 'success' => true]);
+        }
+
+        return response()->json(['message' => 'Password mismatch!', 'success' => false], 406);
+
     }
 }
--- resources/views/pages/user/change-password.blade.php
+++ resources/views/pages/user/change-password.blade.php
@@ -5,9 +5,82 @@
         <h2>Change Password</h2>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
+    <div class="d-flex justify-content-center align-items-center">
+        <div class="w-50">
+            <div class="alert alert-danger d-none" id="errors"></div>
+            <div class="mb-3 row">
+                <label for="old_password" class="col-sm-4 col-form-label">Old Password</label>
+                <div class="col-sm-8">
+                    <input type="password" class="form-control" id="old_password" placeholder="Old password">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="new_password" class="col-sm-4 col-form-label">New Password</label>
+                <div class="col-sm-8">
+                    <input type="password" class="form-control" id="new_password" placeholder="New password">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="confirm_password" class="col-sm-4 col-form-label">Confirm Password</label>
+                <div class="col-sm-8">
+                    <input type="password" class="form-control" id="confirm_password" placeholder="Confirm password">
+                </div>
+            </div>
+            <div class="float-end">
+                <button type="button" class="btn btn-dark me-2" id="updateBtn">Update</button>
+                <button type="button" class="btn btn-outline-dark" id="clearBtn">Clear</button>
+            </div>
         </div>
     </div>
 @endsection
+@push('script')
+    <script>
+        $(document).ready(function () {
+
+            $('#clearBtn').click(function () {
+                clearForm();
+            });
+
+            $('#updateBtn').click(function () {
+                updatePassword();
+            });
+
+        });
+
+        async function clearForm() {
+            $('#old_password').val(null);
+            $('#new_password').val(null);
+            $('#confirm_password').val(null);
+
+        }
+        async function updatePassword() {
+            await $.ajax({
+                method: 'POST',
+                url: `{{route('users.change-password')}}`,
+                data: {
+                    'old_password': $('#old_password').val(),
+                    'new_password': $('#new_password').val(),
+                    'confirm_password': $('#confirm_password').val(),
+                }
+            }).done(function (response) {
+                if(response?.success === true){
+                    Toast.fire({icon: 'success', title: response?.message});
+                }
+                clearForm();
+                $('#errors').addClass('d-none');
+            }).catch(function (err) {
+                clearForm();
+                if(err?.responseJSON?.message){
+                    return $('#errors').html(err?.responseJSON?.message).removeClass('d-none');
+                }
+                let errors = err.responseJSON.errors;
+                let errorString = '';
+                $.each(errors, function(key, value) {
+                    errorString += value + '<br>';
+                });
+                $('#errors').html(errorString).removeClass('d-none');
+            });
+        }
+    </script>
+@endpush
 
